137f29e61d5a5930514c5d4f80b8c7cbb05b5169
OliverSoucc <oliv572v@easv365.dk>
2021-11-26 17:32:14 +0100

    Song class

--- project/src/be/Song.java
+++ project/src/be/Song.java
@@ -1,4 +1,53 @@
 package be;
 
 public class Song {
+    private final int id;
+    private String title;
+    private String artist;
+    private String category;
+    private int time;
+
+    public Song(int id,String title, String artist, String category, int time){
+        this.id = id;
+        this.title = title;
+        this.artist = artist;
+        this.category = category;
+        this.time = time;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
 }
