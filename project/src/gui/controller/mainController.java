d30a2f29b79037ad9ab4f6b98f16170eb5651cfd
silamin <amine.aouina.lp.4@gmail.com>
2021-11-29 11:43:19 +0100

    javafx

--- project/src/gui/controller/mainController.java
+++ project/src/gui/controller/mainController.java
@@ -1,4 +1,49 @@
 package gui.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.SplitMenuButton;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class mainController {
+    public SplitMenuButton category;
+
+    public void newSong(ActionEvent actionEvent) throws IOException {
+        Parent root;
+        root = FXMLLoader.load(getClass().getResource("/gui/view/newSong.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Create a song");
+        stage.setScene(new Scene(root));
+        stage.show();
+
+    }
+
+    public void newPlayList(ActionEvent actionEvent) throws IOException {
+        Parent root;
+        root = FXMLLoader.load(getClass().getResource("/gui/view/newPlayList.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Create a playlist");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public void rockMusic(ActionEvent actionEvent) {
+        category.setText("Rock Music");
+    }
+
+    public void rythmAndBlues(ActionEvent actionEvent) {
+        category.setText("Rythm and blues");
+    }
+
+    public void reggae(ActionEvent actionEvent) {
+        category.setText("Reggae");
+    }
+
+    public void jazz(ActionEvent actionEvent) {
+        category.setText("Jazz");
+    }
 }
