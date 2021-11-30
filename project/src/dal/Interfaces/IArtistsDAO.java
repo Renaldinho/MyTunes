89b7ae4e42b9d7c78eac76c7e23b6943e67f8f01
silamin <amine.aouina.lp.4@gmail.com>
2021-11-30 01:45:52 +0100

    added a comment

--- project/src/dal/Interfaces/IArtistsDAO.java
+++ project/src/dal/Interfaces/IArtistsDAO.java
@@ -6,6 +6,7 @@ import be.Artist;
 import java.sql.SQLException;
 
 public interface IArtistsDAO {
+    //looks for a given artist and returns his/her name if not found creates one and returns the generated key same goes with categories.
     int createArtist(String name) throws SQLException;
     void deleteArtist(String name) throws SQLException;
 
